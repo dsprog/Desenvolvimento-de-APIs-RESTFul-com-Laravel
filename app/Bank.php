ffaea9f4ae80ba5a6ea3e6f02a8dc470eb1af5ab
Daniel Henrique dos Santos Oliveira <dsprog@gmail.com>
2021-01-28 09:06:49 -0300

    Improvement to the ApiControllerTrait
    Improvement to the ApiControllerTrait method, the user can define if he wants to display relationships

--- app/Bank.php
+++ app/Bank.php
@@ -8,4 +8,8 @@ class Bank extends Model
 {
     protected $table = 'banks';
     protected $fillable = ['title', 'code'];
+    
+    public function account(){
+        return $this->hasMany('App\Account');
+    }
 }
